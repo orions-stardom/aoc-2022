3f65631e87f3197e3fa715c7199a8dd148781499
Lennon Cook <lvc@codetripping.net>
2022-12-02 21:28:30 +1100

    get the modular arithmetic working for cleaner day 2

--- day_02.py
+++ day_02.py
@@ -14,23 +14,19 @@ def part_1(*lines):
     score = 0
     for line in lines:
         plays = line.split()
-        op_play =  "ABC".index(plays[0])
-        you_play = "XYZ".index(plays[1])
-        
-        score += you_play + 1
-        you="rock paper scissors".split()[you_play]
-        op="rock paper scissors".split()[op_play]
-        if you_play == op_play:
+        opponent =  "ABC".index(plays[0])
+        you = "XYZ".index(plays[1])
+       
+        # we have plays is 0-2 but need to inc score by 1-3
+        score += you + 1
+        if you == opponent:
             # draw
             score += 3
 
-        elif you == "paper" and op == "rock" or \
-             you == "scissors" and op == "paper" or \
-             you == "rock" and op == "scissors":
-            # win
+        needed = (opponent + 1) % 3
+        if you == needed:
             score += 6
-
-
+    
     return score
 
 def part_2(*lines):
@@ -46,29 +42,15 @@ def part_2(*lines):
     score = 0
     for line in lines:
         plays = line.split()
-        op_play =  "ABC".index(plays[0])
+        opponent =  "ABC".index(plays[0])
         result = plays[1]
         
-        op="rock paper scissors".split()[op_play]
         if result == "X":
-            # need to lose
-            if op == "rock": # we play scissors
-                score += 3
-            if op == "paper":
-                score += 1 # we play rock
-            if op == "scissors":
-                score += 2 # we play paper
-
+            score += (opponent - 1) % 3 + 1
         elif result == "Y":
-            # draw
-            score += op_play +1 + 3
-
+            score += (opponent + 1) + 3
         elif result == "Z":
-            score += 6
-            if op == "rock": # we play paper
-                score += 2
-            if op == "paper": # we play scissors
-                score += 3
-            if op == "scissors": # we play rock
-                score += 1
+            you = (opponent + 1) % 3
+            score += (you + 1) + 6
+    
     return score
