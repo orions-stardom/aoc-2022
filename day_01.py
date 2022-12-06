53a15cbf2f7aa6850975f3bfbdd578d62698791a
Lennon Cook <lvc@codetripping.net>
2022-12-06 17:21:09 +1100

    Drop all the mostly unused argparse stuff

--- day_01.py
+++ day_01.py
@@ -49,34 +49,13 @@ def part_2(*elves):
     return sum(heapq.nlargest(3, (sum(e) for e in elves)))
 
 if __name__ == "__main__":
-    import argparse
     import aocd
     import doctest
     import sys
      
-    cli = argparse.ArgumentParser()
-    all_parts = []
-    try:
-        all_parts.append(part_1) 
-    except NameError:
-        pass
-
-    try:
-        all_parts.append(part_2)
-    except NameError:
-        pass
-
-    cli.add_argument("--part", action="append", choices=all_parts, default=all_parts, dest="do_parts")
-    cli.add_argument("--all-parts", action="store_const", const=all_parts, dest="do_parts")
-    cli.add_argument("--skip-tests", action="store_true")
-    cli.add_argument("--no-submit", action="store_false", dest="submit")
-
-    args = cli.parse_args()
-
-    if not args.skip_tests:
-        failure, tests = doctest.testmod()
-        if failure > 0:
-            sys.exit(f"Failed {failure}/{tests} tests")
+    failure, tests = doctest.testmod()
+    if failure > 0:
+        sys.exit(f"Failed {failure}/{tests} tests")
    
     # aocd has some magic introspection but it doesnt like my naming conventions
     from pathlib import Path
@@ -85,11 +64,15 @@ if __name__ == "__main__":
         year=f.parent.name.removeprefix("aoc-"),
         day=int(f.stem.removeprefix("day_")))
     
-    for part in args.do_parts: 
-        solution = part(*_parse(puzzle_input))
-        part_number = int(part.__name__.removeprefix("part_"))
-        print(f"Solution to part {part_number}: ", solution, sep="\n")
-        if args.submit:
-            # aocd uses parts a and b for some reason, even though AOC uses parts One and Two
-            aocd.submit(solution, part='ab'[part_number-1], reopen=False)
+    for part in 1, 2:
+        try:
+            impl = globals()[f"part_{part}"]
+        except NameError:
+            print(f"No part {part} - skipping")
+            continue
+
+        solution = impl(*_parse(puzzle_input))
+        print(f"Solution to part {part}: ", solution, sep="\n")
+        # aocd uses parts a and b for some reason, even though AOC uses parts One and Two
+        aocd.submit(solution, part='ab'[part-1], reopen=False)
 
