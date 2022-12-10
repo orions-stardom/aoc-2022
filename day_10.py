a6d84eb69e886bbe7aa9a5c62af98765e8251ce5
Lennon Cook <lvc@codetripping.net>
2022-12-11 09:47:53 +1100

    Refactor tests into the class we already have

--- day_10.py
+++ day_10.py
@@ -1,9 +1,168 @@
 #!/usr/bin/env -S pdm run python
 
 def _parse(rawdata):
-    return rawdata.splitlines() 
+    return [Device(rawdata)]
 
 class Device:
+    r"""
+    >>> d = Device('''\
+    ... addx 15
+    ... addx -11
+    ... addx 6
+    ... addx -3
+    ... addx 5
+    ... addx -1
+    ... addx -8
+    ... addx 13
+    ... addx 4
+    ... noop
+    ... addx -1
+    ... addx 5
+    ... addx -1
+    ... addx 5
+    ... addx -1
+    ... addx 5
+    ... addx -1
+    ... addx 5
+    ... addx -1
+    ... addx -35
+    ... addx 1
+    ... addx 24
+    ... addx -19
+    ... addx 1
+    ... addx 16
+    ... addx -11
+    ... noop
+    ... noop
+    ... addx 21
+    ... addx -15
+    ... noop
+    ... noop
+    ... addx -3
+    ... addx 9
+    ... addx 1
+    ... addx -3
+    ... addx 8
+    ... addx 1
+    ... addx 5
+    ... noop
+    ... noop
+    ... noop
+    ... noop
+    ... noop
+    ... addx -36
+    ... noop
+    ... addx 1
+    ... addx 7
+    ... noop
+    ... noop
+    ... noop
+    ... addx 2
+    ... addx 6
+    ... noop
+    ... noop
+    ... noop
+    ... noop
+    ... noop
+    ... addx 1
+    ... noop
+    ... noop
+    ... addx 7
+    ... addx 1
+    ... noop
+    ... addx -13
+    ... addx 13
+    ... addx 7
+    ... noop
+    ... addx 1
+    ... addx -33
+    ... noop
+    ... noop
+    ... noop
+    ... addx 2
+    ... noop
+    ... noop
+    ... noop
+    ... addx 8
+    ... noop
+    ... addx -1
+    ... addx 2
+    ... addx 1
+    ... noop
+    ... addx 17
+    ... addx -9
+    ... addx 1
+    ... addx 1
+    ... addx -3
+    ... addx 11
+    ... noop
+    ... noop
+    ... addx 1
+    ... noop
+    ... addx 1
+    ... noop
+    ... noop
+    ... addx -13
+    ... addx -19
+    ... addx 1
+    ... addx 3
+    ... addx 26
+    ... addx -30
+    ... addx 12
+    ... addx -1
+    ... addx 3
+    ... addx 1
+    ... noop
+    ... noop
+    ... noop
+    ... addx -9
+    ... addx 18
+    ... addx 1
+    ... addx 2
+    ... noop
+    ... noop
+    ... addx 9
+    ... noop
+    ... noop
+    ... noop
+    ... addx -1
+    ... addx 2
+    ... addx -37
+    ... addx 1
+    ... addx 3
+    ... noop
+    ... addx 15
+    ... addx -21
+    ... addx 22
+    ... addx -6
+    ... addx 1
+    ... noop
+    ... addx 2
+    ... addx 1
+    ... noop
+    ... addx -10
+    ... noop
+    ... noop
+    ... addx 20
+    ... addx 1
+    ... addx 2
+    ... addx 2
+    ... addx -6
+    ... addx -11
+    ... noop
+    ... noop
+    ... noop
+    ... ''')
+    >>> d.total_signal
+    13140
+    >>> print(d.picture)
+    ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  
+    ###   ###   ###   ###   ###   ###   ### 
+    ####    ####    ####    ####    ####    
+    #####     #####     #####     #####     
+    ######      ######      ######      ####
+    #######       #######       #######     
+    """
     def __init__(self, instructions):
         self.X = 1
         self.cycle = 0
@@ -11,7 +170,7 @@ class Device:
 
         self.pixels = [[" "]*40 for _ in range(6)]
 
-        for instruction in instructions:
+        for instruction in instructions.splitlines():
             if instruction == "noop":
                 self.run_cycle()
             elif instruction.startswith("addx"):
@@ -35,319 +194,11 @@ class Device:
     def picture(self):
         return "\n".join("".join(row) for row in self.pixels)
 
-def part_1(*lines):
-    r"""
-    >>> part_1(*_parse('''\
-    ... addx 15
-    ... addx -11
-    ... addx 6
-    ... addx -3
-    ... addx 5
-    ... addx -1
-    ... addx -8
-    ... addx 13
-    ... addx 4
-    ... noop
-    ... addx -1
-    ... addx 5
-    ... addx -1
-    ... addx 5
-    ... addx -1
-    ... addx 5
-    ... addx -1
-    ... addx 5
-    ... addx -1
-    ... addx -35
-    ... addx 1
-    ... addx 24
-    ... addx -19
-    ... addx 1
-    ... addx 16
-    ... addx -11
-    ... noop
-    ... noop
-    ... addx 21
-    ... addx -15
-    ... noop
-    ... noop
-    ... addx -3
-    ... addx 9
-    ... addx 1
-    ... addx -3
-    ... addx 8
-    ... addx 1
-    ... addx 5
-    ... noop
-    ... noop
-    ... noop
-    ... noop
-    ... noop
-    ... addx -36
-    ... noop
-    ... addx 1
-    ... addx 7
-    ... noop
-    ... noop
-    ... noop
-    ... addx 2
-    ... addx 6
-    ... noop
-    ... noop
-    ... noop
-    ... noop
-    ... noop
-    ... addx 1
-    ... noop
-    ... noop
-    ... addx 7
-    ... addx 1
-    ... noop
-    ... addx -13
-    ... addx 13
-    ... addx 7
-    ... noop
-    ... addx 1
-    ... addx -33
-    ... noop
-    ... noop
-    ... noop
-    ... addx 2
-    ... noop
-    ... noop
-    ... noop
-    ... addx 8
-    ... noop
-    ... addx -1
-    ... addx 2
-    ... addx 1
-    ... noop
-    ... addx 17
-    ... addx -9
-    ... addx 1
-    ... addx 1
-    ... addx -3
-    ... addx 11
-    ... noop
-    ... noop
-    ... addx 1
-    ... noop
-    ... addx 1
-    ... noop
-    ... noop
-    ... addx -13
-    ... addx -19
-    ... addx 1
-    ... addx 3
-    ... addx 26
-    ... addx -30
-    ... addx 12
-    ... addx -1
-    ... addx 3
-    ... addx 1
-    ... noop
-    ... noop
-    ... noop
-    ... addx -9
-    ... addx 18
-    ... addx 1
-    ... addx 2
-    ... noop
-    ... noop
-    ... addx 9
-    ... noop
-    ... noop
-    ... noop
-    ... addx -1
-    ... addx 2
-    ... addx -37
-    ... addx 1
-    ... addx 3
-    ... noop
-    ... addx 15
-    ... addx -21
-    ... addx 22
-    ... addx -6
-    ... addx 1
-    ... noop
-    ... addx 2
-    ... addx 1
-    ... noop
-    ... addx -10
-    ... noop
-    ... noop
-    ... addx 20
-    ... addx 1
-    ... addx 2
-    ... addx 2
-    ... addx -6
-    ... addx -11
-    ... noop
-    ... noop
-    ... noop
-    ... '''))
-    13140
-    """
-    return Device(lines).total_signal
+def part_1(d):
+    return d.total_signal
 
-
-def part_2(*lines):
-    r"""
-    >>> part_2(*_parse('''\
-    ... addx 15
-    ... addx -11
-    ... addx 6
-    ... addx -3
-    ... addx 5
-    ... addx -1
-    ... addx -8
-    ... addx 13
-    ... addx 4
-    ... noop
-    ... addx -1
-    ... addx 5
-    ... addx -1
-    ... addx 5
-    ... addx -1
-    ... addx 5
-    ... addx -1
-    ... addx 5
-    ... addx -1
-    ... addx -35
-    ... addx 1
-    ... addx 24
-    ... addx -19
-    ... addx 1
-    ... addx 16
-    ... addx -11
-    ... noop
-    ... noop
-    ... addx 21
-    ... addx -15
-    ... noop
-    ... noop
-    ... addx -3
-    ... addx 9
-    ... addx 1
-    ... addx -3
-    ... addx 8
-    ... addx 1
-    ... addx 5
-    ... noop
-    ... noop
-    ... noop
-    ... noop
-    ... noop
-    ... addx -36
-    ... noop
-    ... addx 1
-    ... addx 7
-    ... noop
-    ... noop
-    ... noop
-    ... addx 2
-    ... addx 6
-    ... noop
-    ... noop
-    ... noop
-    ... noop
-    ... noop
-    ... addx 1
-    ... noop
-    ... noop
-    ... addx 7
-    ... addx 1
-    ... noop
-    ... addx -13
-    ... addx 13
-    ... addx 7
-    ... noop
-    ... addx 1
-    ... addx -33
-    ... noop
-    ... noop
-    ... noop
-    ... addx 2
-    ... noop
-    ... noop
-    ... noop
-    ... addx 8
-    ... noop
-    ... addx -1
-    ... addx 2
-    ... addx 1
-    ... noop
-    ... addx 17
-    ... addx -9
-    ... addx 1
-    ... addx 1
-    ... addx -3
-    ... addx 11
-    ... noop
-    ... noop
-    ... addx 1
-    ... noop
-    ... addx 1
-    ... noop
-    ... noop
-    ... addx -13
-    ... addx -19
-    ... addx 1
-    ... addx 3
-    ... addx 26
-    ... addx -30
-    ... addx 12
-    ... addx -1
-    ... addx 3
-    ... addx 1
-    ... noop
-    ... noop
-    ... noop
-    ... addx -9
-    ... addx 18
-    ... addx 1
-    ... addx 2
-    ... noop
-    ... noop
-    ... addx 9
-    ... noop
-    ... noop
-    ... noop
-    ... addx -1
-    ... addx 2
-    ... addx -37
-    ... addx 1
-    ... addx 3
-    ... noop
-    ... addx 15
-    ... addx -21
-    ... addx 22
-    ... addx -6
-    ... addx 1
-    ... noop
-    ... addx 2
-    ... addx 1
-    ... noop
-    ... addx -10
-    ... noop
-    ... noop
-    ... addx 20
-    ... addx 1
-    ... addx 2
-    ... addx 2
-    ... addx -6
-    ... addx -11
-    ... noop
-    ... noop
-    ... noop
-    ... '''))
-    ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  
-    ###   ###   ###   ###   ###   ###   ### 
-    ####    ####    ####    ####    ####    
-    #####     #####     #####     #####     
-    ######      ######      ######      ####
-    #######       #######       #######     
-    """
-    print(Device(lines).picture)
+def part_2(d):
+    print(d.picture)
 
 if __name__ == "__main__":
     import aocd
