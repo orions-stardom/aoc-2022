d51530a82507f7e77c302207517246648e071528
Lennon Cook <lvc@codetripping.net>
2022-12-11 19:31:01 +1100

    Refactor to avoid horrible no good buggy global state
    Not for the first time in an AoC puzzle, I lost over 20 minutes
    on part 2 despite my solve logic being right and it working on the
    example, because I carried over the solved state from part 1..

--- day_11.py
+++ day_11.py
@@ -2,11 +2,13 @@
 import parse
 import heapq
 import math
+from dataclasses import dataclass
+from typing import NamedTuple, Callable
 
-
+@dataclass
 class Monkey:
     """
-    >>> m = Monkey('''\
+    >>> m = Monkey.parse('''\
     ... Monkey 0:
     ...   Starting items: 79, 98
     ...   Operation: new = old * 19
@@ -26,41 +28,46 @@ class Monkey:
     3
 
     """
-    def __init__(self, description:str):
-        lines = description.splitlines()[1:]
-        self.items = [r[0] for r in parse.findall("{:d}", lines[0])]
-        self.operation = eval(lines[1].replace("Operation: new =", "lambda old:"))
-        self.divisor = parse.search("{:d}", lines[2])[0]
-        self.true_target = parse.search("{:d}", lines[3])[0] 
-        self.false_target = parse.search("{:d}", lines[4])[0] 
-        self.items_touched = 0
+    items: list[int]
+    operation: Callable[int,int]
+    divisor: int
+    true_target: int
+    false_target: int
+    items_touched: int = 0
 
-    def inspect(self, relief_ratio:int):
-        for item in self.items:
-            new_worry_level = (self.operation(item) // relief_ratio)  % math.prod(m.divisor for m in monkies)
-            target = self.true_target if new_worry_level % self.divisor == 0 else self.false_target
-            monkies[target].items.append(new_worry_level)
-            self.items_touched += 1
-            
-        self.items.clear()
+    @classmethod
+    def parse(cls, data):
+        lines = data.splitlines()[1:]
+        return cls(
+            [r[0] for r in parse.findall("{:d}", lines[0])],
+            eval(lines[1].replace("Operation: new =", "lambda old:")),
+            parse.search("{:d}", lines[2])[0],
+            parse.search("{:d}", lines[3])[0],
+            parse.search("{:d}", lines[4])[0],
+        )
 
-def run(nrounds=20, *, relief_ratio:int):
+
+def run_sim(rawdata:str, *, nrounds:int, relief_ratio:int):
+    monkeys = [Monkey.parse(data) for data in rawdata.split("\n\n")]
+    lcm = math.prod(m.divisor for m in monkeys) # all the divisors turn out to be prime
+
     for _ in range(nrounds):
-        for monkey in monkies:
-            monkey.inspect(relief_ratio)
+        for monkey in monkeys:
+            for item in monkey.items:
+                new_worry_level = (monkey.operation(item) // relief_ratio) % lcm
+                target = monkey.true_target if new_worry_level % monkey.divisor == 0 else monkey.false_target
+                monkeys[target].items.append(new_worry_level)
+                monkey.items_touched += 1
 
-monkies = []
-def parse_monkies(data:str):
-    global monkies
-    monkies = []
+            monkey.items.clear()
 
-    for description in data.split("\n\n"):
-        # assume they're in order.. 
-        monkies.append(Monkey(description))
+    m1, m2 = heapq.nlargest(2, monkeys, key=lambda m: m.items_touched)
+    return m1.items_touched * m2.items_touched
 
-def part_1():
+
+def part_1(data:str):
     """
-    >>> parse_monkies('''\
+    >>> part_1('''\
     ... Monkey 0:
     ...   Starting items: 79, 98
     ...   Operation: new = old * 19
@@ -89,18 +96,14 @@ def part_1():
     ...     If true: throw to monkey 0
     ...     If false: throw to monkey 1
     ... ''')
-    >>> part_1()
     10605
     """
-    run(relief_ratio=3)
-
-    m1, m2 = heapq.nlargest(2, monkies, key=lambda m: m.items_touched)
-    return m1.items_touched * m2.items_touched
+    return run_sim(data, nrounds=20, relief_ratio=3)
 
 
-def part_2():
+def part_2(data:str):
     """
-    >>> parse_monkies('''\
+    >>> part_2('''\
     ... Monkey 0:
     ...   Starting items: 79, 98
     ...   Operation: new = old * 19
@@ -129,13 +132,9 @@ def part_2():
     ...     If true: throw to monkey 0
     ...     If false: throw to monkey 1
     ... ''')
-    >>> part_2()
     2713310158
     """ 
-    run(nrounds=10_000, relief_ratio=1)
-
-    m1, m2 = heapq.nlargest(2, monkies, key=lambda m: m.items_touched)
-    return m1.items_touched * m2.items_touched
+    return run_sim(data, nrounds=10_000, relief_ratio=1)
 
 if __name__ == "__main__":
     import aocd
@@ -155,13 +154,12 @@ if __name__ == "__main__":
    
 
     for part in 1, 2:
-        parse_monkies(puzzle_input)
         try:
             impl = globals()[f"part_{part}"]
         except KeyError:
             print(f"No part {part} - skipping")
             continue
-        solution = impl()
+        solution = impl(puzzle_input)
         if solution is not None:
             print(f"Solution to part {part}: ", solution, sep="\n")
             # aocd uses parts a and b for some reason, even though AOC uses parts One and Two
